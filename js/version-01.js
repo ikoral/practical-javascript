17d8f48e367e4985d08fac99162e8e8bec6e57a6
ikoral <ilhankoral@outlook.com>
2019-11-21 15:36:01 +0100

    version 3 is created, interval push

--- js/version-01.js
+++ js/version-01.js
@@ -2,8 +2,8 @@
  *************VERSION-01 **************
  ***************ARRAYS*****************
  **************************************/
-{
-  const pageTitle = "Arrays";
+/*{
+  const pageTitle = "Practical Javascript";
   const header = document.createElement("h2");
   header.innerHTML = pageTitle;
   document.getElementById("content").appendChild(header);
@@ -11,50 +11,45 @@
   console.groupCollapsed("%c" + pageTitle, "font-weight:bold");
 }
 
-//Store todos
-var todos = ["item 1", "item 2", "item 3"];
-
-function arrayExample() {
-  console.log(" *****VERSION-01-Arrays*****");
-
-  //Display todos
-  console.log("My Todos:", todos);
-
-  //Add new todos
-  todos.push("item 4");
-  todos.push("item 5");
-  console.log("My Todos after 2 times Push:", todos);
-
-  //Change item in existing todos
-  todos[0] = "item 1 updated";
-  console.log("My Todos after first item updated:", todos);
-
-  //Delete item in existing todos
-  todos.splice(0, 1); //from first item(index) one item delete.
-  console.log("My Todos; first item deleted:", todos);
-  todos.splice(3, 1); //from third item(index) one item delete.
-  console.log("My Todos; last item deleted:", todos);
-
-  /********Additional Examples for Array*****************/
-
-  //Add item as first item to todos.
-  var unshiftedItem = todos.unshift("item 1");
-  console.log("My Todos; first item unshifted again:", todos);
-  console.log("unShifted item:", todos[0]);
-
-  //Remove first item from todos.
-  var shiftedItem = todos.shift();
-  console.log("My Todos; first item deleted with shift:", todos);
-  console.log("Shifted item:", shiftedItem);
-}
-arrayExample();
-arrayExample.display = true;
-evaluate(arrayExample);
-
 {
   document.body.appendChild(document.createElement("hr"));
   console.groupEnd();
-}
+}*/
+
+//Store todos
+var todos = ["item 1", "item 2", "item 3"];
+
+console.log(" *****VERSION-01-Arrays*****");
+
+//Display todos
+console.log("My Todos:", todos);
+
+//Add new todos
+todos.push("item 4");
+todos.push("item 5");
+console.log("My Todos after 2 times Push:", todos);
+
+//Change item in existing todos
+todos[0] = "item 1 updated";
+console.log("My Todos after first item updated:", todos);
+
+//Delete item in existing todos
+todos.splice(0, 1); //from first item(index) one item delete.
+console.log("My Todos; first item deleted:", todos);
+todos.splice(3, 1); //from third item(index) one item delete.
+console.log("My Todos; last item deleted:", todos);
+
+/********Additional Examples for Array*****************/
+
+//Add item as first item to todos.
+var unshiftedItem = todos.unshift("item 1");
+console.log("My Todos; first item unshifted again:", todos);
+console.log("unShifted item:", todos[0]);
+
+//Remove first item from todos.
+var shiftedItem = todos.shift();
+console.log("My Todos; first item deleted with shift:", todos);
+console.log("Shifted item:", shiftedItem);
 
 /*********************************************
  ****************Version Control***************
---
+++ js/version-03.js
@@ -0,0 +1,17 @@
+/*************************************
+ *************VERSION-03 **************
+ **************OBJECTS*****************
+ **************************************/
+
+var myComputer = {
+  operatingSystem: "mac",
+  screenSize: "15 inches",
+  purchaseYear: 2011
+};
+/*****evaluates********/
+evaluate(FUNCTIONS);
+evaluate(sayHiTo);
+evaluate(displayTodos);
+evaluate(addTodo);
+evaluate(changeTodo);
+evaluate(deleteTodo);
